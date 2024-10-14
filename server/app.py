1ab79cfccb2d5a91930297e6d4b069dad655401f
josephbill <josephbill00@gmail.com>
2024-10-14 10:13:14 +0300

    added flask swagger ui

--- server/app.py
+++ server/app.py
@@ -224,4 +224,5 @@ def upload_profile_picture(user_id):
         
 
 if __name__ == '__main__':
-    app.run(port=3000,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port,debug=True)
